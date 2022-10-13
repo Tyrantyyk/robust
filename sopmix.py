64a84ede595262f61162ff4e20adf28a5a48f85a
Tyrantyyk <1765132121@qq.com>
2022-10-13 13:12:44 +0000

    noisy for sop

--- sopmix.py
+++ sopmix.py
@@ -193,7 +193,7 @@ def train(epoch, net, ema_net, optimizer, labeled_trainloader):
         # fmixup loss
         loss_cr = CEsoft(outputs_x2[idx_chosen], targets=pseudo_label_l[idx_chosen]).mean()
         # consistency loss
-        # loss_ce = CEsoft(outputs_x[idx_chosen], targets=pseudo_label_l[idx_chosen]).mean()
+        loss_ce = CEsoft(outputs_x[idx_chosen], targets=pseudo_label_l[idx_chosen]).mean()
         # above: loss for reliable samples
 
         loss_net1 = w * (loss_cr + loss_mix + loss_fmix)
