89516a1b541edb1dbc6b952dfa936228a082e8e1
Tyrantyyk <1765132121@qq.com>
2022-10-14 06:05:36 +0000

    three stage v1

--- losses.py
+++ losses.py
@@ -5,6 +5,45 @@ import torch.nn.functional as F
 import math
 import random
 
+def get_negative_mask(batch_size):
+    negative_mask = torch.ones((batch_size, 2 * batch_size), dtype=bool)
+    for i in range(batch_size):
+        negative_mask[i, i] = 0
+        negative_mask[i, i + batch_size] = 0
+
+    negative_mask = torch.cat((negative_mask, negative_mask), 0)
+    return negative_mask
+
+def cl_hard_loss(out_1, out_2, tau_plus, batch_size, beta, estimator, temperature):
+    # neg score
+    out = torch.cat([out_1, out_2], dim=0)
+    neg = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+    old_neg = neg.clone()
+    mask = get_negative_mask(batch_size).cuda()
+    neg = neg.masked_select(mask).view(2 * batch_size, -1)
+
+    # pos score
+    pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+    pos = torch.cat([pos, pos], dim=0)
+
+    # negative samples similarity scoring
+    if estimator == 'hard':
+        N = batch_size * 2 - 2
+        imp = (beta * neg.log()).exp()
+        reweight_neg = (imp * neg).sum(dim=-1) / imp.mean(dim=-1)
+        Ng = (-tau_plus * N * pos + reweight_neg) / (1 - tau_plus)
+        # constrain (optional)
+        Ng = torch.clamp(Ng, min=N * np.e ** (-1 / temperature))
+    elif estimator == 'easy':
+        Ng = neg.sum(dim=-1)
+    else:
+        raise Exception('Invalid estimator selected. Please use any of [hard, easy]')
+
+    # contrastive loss
+    loss = (- torch.log(pos / (pos + Ng))).mean()
+
+    return loss
+
 class sop_trans_mat_loss(nn.Module):
     def __init__(self, num_examp, num_classes=10, Tr=0, ratio_balance=0):
         super(sop_trans_mat_loss, self).__init__()
